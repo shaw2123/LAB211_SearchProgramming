da1439abf3d97425c8c85a030ab54f43767ff1d0
shaw2123 <139949617+shaw2123@users.noreply.github.com>
2023-09-09 11:41:31 +0700

    Add files via upload

---
+++ lab/src/Common/Algorithm.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Common;
+
+/**
+ *
+ * @author ACER
+ */
+public class Algorithm {
+
+    public void buddleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public int partition(int[] array, int left, int right) {
+        //exam: 1 12 5 [7] 6 9 2 : 7 is pivot
+        //Run from left: i - Run from right: j
+        int i = left, j = right;
+        //Instance a variable: pivot - trục
+        int pivot = array[(left + right) / 2];
+        //Create a temp variable
+        int tmp;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                //Execute swap between value of index [i] and value of index [j]
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                i++;
+                j--;
+            }
+        };
+        return i;
+    }
+    public int linearSearch(int[] arr, int target) {
+    for(int i = 0; i < arr.length; i++) {
+        if(arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+    public int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // target not found
+    }
+}
